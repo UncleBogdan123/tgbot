f5568e0d366e9b0231899c7fe508001bdb1ddebe
UncleBogdan123 <asanw210@gmail.com>
2025-03-31 21:48:06 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time, threading, schedule
 import telebot
 from config import token
 from botlogic import gen_pass
@@ -27,33 +26,29 @@ def send_password(message):
 def send_result_coin(message):
     bot.send_message(message.chat.id, coin('flip'))
 
-@bot.message_handler(commands=['help'])
-def send_welcome(message):
-    bot.reply_to(message, "Hi! Use /set <seconds> to set a timer")
+@bot.message_handler(commands=["poll"])
+def create_poll(message):
+    bot.send_message(message.chat.id, "English Article Test")
+    answer_options = ["a", "an", "the", "-"]
+
+    bot.send_poll(
+        chat_id=message.chat.id,
+        question="We are going to '' park.",
+        options=answer_options,
+        type="quiz",
+        correct_option_id=2,
+        is_anonymous=False,
+    )
 
 
-def beep(chat_id) -> None:
-    """Send the beep message."""
-    bot.send_message(chat_id, text='Beep!')
+@bot.poll_answer_handler()
+def handle_poll(poll):
+    # This handler can be used to log User answers and to send next poll
+    pass
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
 
-@bot.message_handler(commands=['set'])
-def set_timer(message):
-    args = message.text.split()
-    if len(args) > 1 and args[1].isdigit():
-        sec = int(args[1])
-        schedule.every(sec).seconds.do(beep, message.chat.id).tag(message.chat.id)
-    else:
-        bot.reply_to(message, 'Usage: /set <seconds>')
-
-
-@bot.message_handler(commands=['unset'])
-def unset_timer(message):
-    schedule.clear(message.chat.id)
-
-
-if __name__ == '__main__':
-    threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+bot.infinity_polling()
