e001c954a97c2b053cac431818cca6ba91d39d33
UncleBogdan123 <asanw210@gmail.com>
2025-04-04 20:51:13 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -40,15 +40,62 @@ def create_poll(message):
         is_anonymous=False,
     )
 
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    help_text = """
+Доступные команды:
+/start - Начать работу с ботом
+/hello - Приветствие
+/bye - Попрощаться
+/generate_password - Сгенерировать случайный пароль (10 символов)
+/Coin_Flip - Подбросить монетку
+/poll - Создать тест на артикли
+/help - Показать это сообщение
+/cs_maps - Карты для игры в КС2
+"""
+    bot.reply_to(message, help_text)
+
+@bot.message_handler(commands=['cs_maps'])
+def maps(message):
+    maps_name= """
+/Aim_maps - Аимки
+/HS_maps - Прятки
+/Guide_maps - Карты с гайдами на что либо
+/Funny_maps - Карты для игры с друзьями
+"""
+    
+    bot.reply_to(message, maps_name)
+
+@bot.message_handler(commands=['Aim_maps'])
+def aim(message):
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=3084291314&searchtext=AIM")
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=594315388&searchtext=AIM")
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=3070244462&searchtext=AIM")
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=1687663948&searchtext=AIM")
+
+@bot.message_handler(commands=['HS_maps'])
+def hide(message):
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=3406406223&searchtext=Hide+and+seek")
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=3326282876&searchtext=Hide+and+seek")
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=3325831241&searchtext=Hide+and+seek")
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=3387070705&searchtext=Hide+and+seek")
+
+@bot.message_handler(commands=['Guide_maps'])
+def guide(message):
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=3379564935&searchtext=AIM")
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=3100869952&searchtext=recoil")
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=3355497176&searchtext=movement")
+
+@bot.message_handler(commands=['Funny_maps'])
+def fun(message):
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=3331706760&searchtext=jetpack+joyrider")
+    bot.reply_to(message,"https://steamcommunity.com/sharedfiles/filedetails/?id=3408062754&searchtext=squid")
+
+
 
 @bot.poll_answer_handler()
 def handle_poll(poll):
     # This handler can be used to log User answers and to send next poll
     pass
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
 
 bot.infinity_polling()
